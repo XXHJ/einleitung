442b043c11fb89708d036d019991faefe7c2f509
xiong.xiao <xiong.xiao@aisppeech.com>
2019-05-21 14:13:00 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '#/layout/header.vue'
+import Footer from '#/layout/footer.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Header,
+    Footer
   }
 }
 </script>
---
+++ vue.config.js
@@ -0,0 +1,44 @@
+
+const path = require('path');
+
+function resolve (dir) {
+    return path.join(__dirname, dir);
+}
+
+module.exports = {
+    chainWebpack (config) {
+        config.resolve.alias
+            .set('~', resolve('src/views'))
+            .set('#', resolve('src/components'))
+            .set('style', resolve('src/assets/style'))
+            .set('svg', resolve('src/assets/svg'))
+            .set('img', resolve('src/assets/img'))
+            .set('icon', resolve('src/assets/icon'))
+            .set('utils', resolve('src/utils'))
+            .set('model', resolve('src/model'))
+            .set('api', resolve('src/api'));
+
+        config.module
+            .rule('svg')
+            .exclude.add(resolve('src/assets/icon'))
+            .end();
+
+        config.module
+            .rule('svg-icon')
+            .test(/\.(svg)(\?.*)?$/)
+            .include.add(resolve('src/assets/icon'))
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({
+                symbolId: 'icon-[name]',
+            })
+            .end()
+            .use('svgo-loader')
+            .loader('svgo-loader')
+            .options({
+                externalConfig: resolve('svgo.yml'),
+            })
+            .end();
+    },
+}
